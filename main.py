a5dfced355793106fa3b09d0ecdc2f823832a877
Delvin Dsouza <61946291+dxdelvin@users.noreply.github.com>
2024-02-04 02:23:12 +0530

    Update main.py
    Add Color Warnings To Text (Still on Develoment)

--- main.py
+++ main.py
@@ -11,14 +11,39 @@ guide_label = Label(text="Write or it will be Disappeared",font=('calibre',15,'b
 guide_label.place(relx=0.5,rely=0.07,anchor="center")
 text_entry = Text(root,width=60,height=23,font=('calibre',10,'normal'))
 text_entry.place(relx=0.5,rely=0.5,anchor="center")
+
+def make_orange():
+    current_value = text_entry.get("1.0", "end-1c")
+    global value
+    if current_value == value:
+        text_entry.config(foreground="orange")
+        root.after(2000, make_red)
+    else:
+        text_entry.config(foreground="black")
+        root.after(2000,dx)
+
+def make_red():
+    current_value = text_entry.get("1.0", "end-1c")
+    global value
+    if current_value == value:
+        text_entry.config(foreground="red")
+        root.after(2000,destroy)
+    else:
+        text_entry.config(foreground="black")
+        root.after(2000,dx)
+
+def destroy():
+    text_entry.delete("1.0", "end")
+    text_entry.insert("end", "Text is Lost")
+    value = "d"
+    text_entry.config(foreground="black")
+
 def dx():
     global value
     current_value = text_entry.get("1.0", "end-1c")
     print(value)
     if current_value == value:
-        text_entry.delete("1.0", "end")
-        text_entry.insert("end", "Text is Lost")
-        root.after(2000, dx)
+        make_orange()
     else:
         value = current_value
         root.after(2000, dx)
@@ -29,7 +54,3 @@ value = "d"
 dx()
 
 root.mainloop()
-
-
-
-
